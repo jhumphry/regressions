731bc1308e94cdb341511618ba5739f6fd37b0b7
James Humphry <git@binecho.co.uk>
2015-10-28 21:43:52 +0000

    Add a base regressions package
    This contains only various imports / definitions that will be useful
    in the child modules and should be able to be used via
    'from .. import *' without issues.

---
+++ regressions/__init__.py
@@ -0,0 +1,23 @@
+# regressions
+
+"""A package which implements various forms of regression."""
+
+import numpy as np
+try:
+    import scipy.linalg as linalg
+    linalg_source = 'scipy'
+except ImportError:
+    import numpy.linalg as linalg
+    linalg_source = 'numpy'
+
+class ParameterError(Exception):
+    """Parameters passed to a regression routine are unacceptable"""
+    pass
+
+# Maximum iterations that will be attempted by iterative routines by
+# default
+DEFAULT_MAX_ITERATIONS = 100
+
+# A default epsilon value used in various places, such as to decide when
+# iterations have converged
+DEFAULT_EPSILON = 0.01
