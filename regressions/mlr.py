b2cefff8012057f2b64ebc2c0c68c6d2cdb858cc
James Humphry <git@binecho.co.uk>
2015-11-06 21:36:56 +0000

    Document MLR routines

--- regressions/mlr.py
+++ regressions/mlr.py
@@ -1,6 +1,4 @@
-# regressions.mlr
-
-"""A package which implements Multiple Linear Regression."""
+"""A module which implements Multiple Linear Regression."""
 
 import random
 
@@ -9,7 +7,33 @@ from . import *
 
 class MLR:
 
-    """Multiple Linear Regression"""
+    """Multiple Linear Regression
+
+    Standard multiple linear regression assumes the relationship between the
+    variables (once the means have been subtracted to center both variables)
+    is Y = A X + E where E is a vector of zero-mean noise vectors.
+
+    Note :
+        The regression matrix B is found using the pseudo-inverse. In order
+        for this to be calculable, the number of calibration samples ``N`` has
+        be be larger than the number of X variables ``n``, and there must not
+        be any collinearities in the calibration X data.
+
+    Args:
+        X (ndarray N x n): X calibration data, one row per data sample
+        Y (ndarray N x m): Y calibration data, one row per data sample
+
+    Attributes:
+        data_samples (int): number of calibration data samples (=N)
+        max_rank (int): maximum rank of calibration X-data
+        X_variables (int): number of X variables (=n)
+        Y_variables (int): number of Y variables (=m)
+        X_offset (float): Offset of calibration X data from zero
+        Y_offset (float): Offset of calibration Y data from zero
+            components (int): number of components extracted (=g)
+        B (ndarray m x n): Resulting regression matrix
+
+    """
 
     def __init__(self, X, Y):
 
@@ -40,6 +64,23 @@ class MLR:
         self.B = linalg.inv(Xc.T @ Xc) @ Xc.T @ Yc
 
     def prediction(self, Z):
+
+        """Predict the output resulting from a given input
+
+        Args:
+            Z (ndarray of floats): The input on which to make the prediction.
+                Must either be a one dimensional array of the same length as
+                the number of calibration X variables, or a two dimensional
+                array with the same number of columns as the calibration X
+                data and one row for each input row.
+
+        Returns:
+            Y (ndarray of floats) : The predicted output - either a one
+            dimensional array of the same length as the number of calibration
+            Y variables or a two dimensional array with the same number of
+            columns as the calibration Y data and one row for each input row.
+        """
+
         if len(Z.shape) == 1:
             if Z.shape[0] != self.X_variables:
                 raise ParameterError('Data provided does not have the same '
