3ce1034f3bf55b98c5d59f163f551c02f802ac33
James Humphry <git@binecho.co.uk>
2015-11-12 19:43:00 +0000

    Implement the RESS statistic

--- regressions/fitstats.py
+++ regressions/fitstats.py
@@ -3,6 +3,58 @@
 from . import *
 
 
+def RESS(R, X, Y, others=None, relative=False):
+    """Implements the Residual Error Sum of Squares
+
+    This function calculates the RESS statistic for a given regression
+    class and a set of calibration data. The regression function is
+    trained on the X and Y data. The X data is then used to predict a set
+    of Y data. The difference between these predictions and the true Y
+    data is squared and summed to give the RESS statistic. Note that this
+    statistic can be misleading if used on its own as it can reward
+    routines which over-fit to the sample data and do not have good
+    generalisation performance. Consider using in conjunction with the
+    :py:func:`PRESS` statistic.
+
+    Args:
+        R (class): A regression class
+        X (ndarray N x n): X calibration data, one row per data sample
+        Y (ndarray N x m): Y calibration data, one row per data sample
+        others (dict, optional): A dict of other parameters to send to the
+            regression class constructor.
+        relative (boolean, optional): whether to divide the error by the
+            true Y value before squaring and summing - where Y columns have
+            different scales this may help to prevent the output being
+            dominated by the column with the largest magnitude.
+
+    Returns:
+        RESS (float): The RESS statistic.
+
+    """
+
+    if others is None:
+        others = {}
+
+    if X.shape[0] != Y.shape[0]:
+        raise ParameterError('X and Y data must have the same number of '
+                             'rows (data samples)')
+
+    # Change 1-D arrays into column vectors
+    if len(X.shape) == 1:
+        X = X.reshape((X.shape[0], 1))
+
+    if len(Y.shape) == 1:
+        Y = Y.reshape((Y.shape[0], 1))
+
+    model = R(X=X, Y=Y, **others)
+    Yhat = model.prediction(Z=X)
+
+    if relative:
+        return (((Yhat - Y) / Y)**2).sum()
+    else:
+        return ((Yhat - Y)**2).sum()
+
+
 def PRESS(R, X, Y, others=None, relative=False):
     """Implements the Predicted Residual Error Sum of Squares
 
